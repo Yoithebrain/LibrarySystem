755442d1944e1cfc02ebe95ffebfe6b55b385e31
Yoithebrain <crazyone5477@gmail.com>
2024-03-13 11:23:41 +0100

    Added a database singleton object, worked on reservation system.

---
+++ src/Database.py
@@ -0,0 +1,53 @@
+# Imports
+import sqlite3
+import time
+# import os
+# Globals
+# Variable to store the connection object
+#db_con = None
+class DatabaseConnection:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._connection = None
+        return cls._instance
+    
+    def connect(self):
+        if self._connection is None:
+            try:
+                # Get the current directory of the script
+                #current_dir = os.path.dirname(os.path.abspath(__file__))
+                # Specify the path to the database file relative to the script
+                #db_path = os.path.join(current_dir, '..', 'LibraryDB.sqlite3')
+                db_path = "C:/Users/KOM/Documents/Uge 6 - Case 1 - Niveau 2/LibrarySystem/LibraryDB.sqlite3"
+                # Connect to SQLite database
+                self._connection = sqlite3.connect(db_path)
+                print("Connect to DB succesfully")
+            except Exception as e:
+                print(f"An error occurred when trying to connect to DB, {e}")
+        return self._connection
+
+    def close(self):
+        if self._connection is not None:
+            try:
+                # Close database connection
+                self._connection.close()
+                self._connection = None
+                print("Database connection close succesfully")
+            except Exception as e:
+                print(f"Error occurred when closing the database connection, {e}")
+
+'''
+# Example usage - Debug lines
+if __name__ == "__main__":
+    # This code block will run only if this script is executed directly
+    
+    # You can put some test code here if needed
+    db_connection = DatabaseConnection()
+    db_connection.connect()
+    time.sleep(5)
+    # Test the connection or perform any other operations
+    db_connection.close()
+'''
---
+++ src/Reservation.py
@@ -0,0 +1,89 @@
+####
+# Handles reservations
+# - CLY 13-03-24 -
+####
+# Imports
+import sqlite3
+import Database
+import datetime
+
+# Class definition
+class Reservation:
+    def __init__(self, conn):
+        self.conn = conn
+    ####
+    # Function that deals with reserving a book, currently a book can only be reserved once. This is a limitation from our side to prevent issue with
+    # multiple active reservations.. As that would mean a different setup for the current database.
+    # - CLY 13-03-24 - 
+    ####
+    def reserve_book(self, user_id, book_id):
+        try:
+            cursor = self.conn.cursor()
+            # Check if a reservation already exists
+            cursor.execute("SELECT COUNT(*) FROM Reservations WHERE bookID = ? AND isFulfilled = 0", (book_id,))
+            active_res_count = cursor.fetchone()[0]
+            if active_res_count == 0:
+                reservationDate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                cursor.execute("INSERT INTO Reservations (userID, bookID, reservationDate) VALUES (?, ?, ?)", (user_id, book_id, reservationDate))
+                self.conn.commit()
+                print("Book has been reserved")
+            else:
+                print("There is already an active reservation for the book in the system currently, please try again at another time")
+        except Exception as e:
+            print(f"Something went wrong when trying to reserve the book, {e}")
+    
+    ###
+    # Checks if a book have been returned where the book may have a reservation
+    # - CLY 13-03-24 -
+    ###
+    def check_reservation_availbility (self, book_id):
+        try:
+            cursor = self.conn.cursor()
+            # Check if the book is currently being borrowed or if the book have been returned
+            cursor.execute("SELECT COUNT(*) FROM BorrowedBooks WHERE bookID = ? and isAvailable = 1", (book_id,))
+            res_count = cursor.fetchone()[0]
+            if res_count == 0:
+                print("Reservation is available")
+                #return res
+            else:
+                print("Book is still not available")
+        except sqlite3.Error as e:
+            print(f"An error occurred when trying to check if a reservation was avaiable to be borrowed: {e}")
+    ####
+    # Deals with fulfilling a reservation when a reservation is borrowed by the user
+    # - CLY 13-02-24 - 
+    ####
+    def isFulfilled (self, book_id, user_id):
+        try:
+            cursor = self.conn.cursor()
+            # Get reservation Id
+            cursor.execute("SELECT reservationID, userID from Reservations WHERE bookID = ? AND isFulfilled = 0 ORDER BY reservationDate DESC LIMIT 1", (book_id,))
+            res = cursor.fetchone()
+            if res:
+                reservation_id, userID = res
+                if userID == user_id:
+                    cursor.execute("UPDATE Reservations SET isFulfilled = 1 WHERE reservationID = ?", (reservation_id,))
+                    self.conn.commit()
+                    print("Reservation have been fulfilled")
+        except sqlite3.Error as e:
+            print(f"An error occurred when trying to fulfill a reservation: {e}")
+        
+
+# Example usage - Debug code lines for unit
+if __name__ == "__main__":
+    # Connect to SQLite database
+    conn = Database.DatabaseConnection()
+    #conn.connect()
+    reservation_system = Reservation(conn.connect())
+
+    # Example usage: Reserve a book
+    reservation_system.reserve_book(1, 1)
+
+    # Example usage: Fulfill a reservation
+    reservation_system.isFulfilled(1, 1)
+
+    # Example usage: Check reservation availability
+    reservation_system.check_reservation_availbility(1)
+
+    # Close the database connection
+    conn.close()
